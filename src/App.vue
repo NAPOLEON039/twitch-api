3aca545334a2e95cb384e3c2c061341a65002d46
NAPOLEON039 <nihar.raote@gmail.com>
2018-05-10 19:42:38 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <MainComponent/>
+  </div>
+</template>
+
+<script>
+import MainComponent from './components/MainComponent'
+
+export default {
+  name: 'App',
+  components: {
+    MainComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
